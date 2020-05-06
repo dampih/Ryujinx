a30aafbe6216e951edc643357c0793d99e8ad81c
mageven <62494521+mageven@users.noreply.github.com>
2020-05-06 10:08:18 +0530

    Initial rebased AddOnContent support
    * Fix bounds calculation
    * Use existing GameCard in VFS per Xpl0itR's suggestion
    + Add dummy IPurchaseEventManager per AcK's suggestion

--- Ryujinx.HLE/HOS/ApplicationLoader.cs
+++ Ryujinx.HLE/HOS/ApplicationLoader.cs
@@ -111,6 +111,8 @@ namespace Ryujinx.HLE.HOS
 
             Xci xci = new Xci(_fileSystem.KeySet, file.AsStorage());
 
+            _contentManager.SetGameCard(file.AsStorage());
+
             if (!xci.HasPartition(XciPartitionType.Secure))
             {
                 Logger.PrintError(LogClass.Loader, "Unable to load XCI: Could not find XCI secure partition");
@@ -143,6 +145,8 @@ namespace Ryujinx.HLE.HOS
             }
 
             _contentManager.LoadEntries(_device);
+
+            _contentManager.AddAocData(securePartition, mainNca.Header.TitleId, ContentPath.GamecardContents);
 
             LoadNca(mainNca, patchNca, controlNca);
         }
